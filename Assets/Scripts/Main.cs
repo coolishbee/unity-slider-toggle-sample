eca46a60fd9071024c228a62ad8525090ae3e290
jameschun7 <chc3484@gmail.com>
2021-12-03 15:47:27 +0900

    [add] demo project

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Main : MonoBehaviour
+{
+    public SlideToggle toggle;
+
+    // Start is called before the first frame update
+    void Start()
+    {        
+        toggle.isOn = false;
+        //toggle.toogleEvent.AddListener((enabled) => Debug.Log("Main : "+enabled));        
+    }    
+
+    public void OnClickBtn(bool enabled)
+    {
+        Debug.Log("OnClickBtn : "+enabled);
+    }
+    
+}
